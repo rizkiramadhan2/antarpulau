c07499d027943df104a1dfbc2f274ff1611cd3a0
rizkiramadhan2 <rizki2500@gmail.com>
2017-09-02 12:54:02 +0700

    add all files

---
+++ application/views/maintenance.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<head>
+  <title>Antar Pulau</title>
+  <link rel="icon" href="<?php echo base_url().'img/antarpulau_icon.png' ?>" >
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/style.css'?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/animate.min.css'?>">
+</head>
+
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<body class="container animated fadeIn">
+  <article class="col-md-12 col-sm-12 col-xs-12 text-center ">
+    <div class = "hidden-xs"> <img src=<?php echo base_url().'img/antarpulau.svg' ?> height="150px"></img> </div>
+    <div class = "hidden-sm hidden-md hidden-lg"> <img src=<?php echo base_url().'img/antarpulau.svg' ?> height="80px"></img> </div>
+    <h2>This site is under construction</h2><hr>
+    <div>
+
+        <p>For more information send us email <a href="#">admin@antarpulaucargo.com</a><br>or contact +6281252548488  </p>
+        <p style="color: grey">&mdash; The Team</p>
+    </div>
+</article>
+</body>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
