7577c043726cb28f2cfd88960a79577ab49dd377
ichwandana <ichwan.dana@gmail.com>
2017-09-07 15:22:44 +0700

    Pembenahan lagi di ">

--- application/views/home.php
+++ application/views/home.php
@@ -44,8 +44,10 @@
     <!-- /FACEBOOK WIDGET -->
     <div class="global-wrap">
         <header id="main-header">
+
             <div class="header-top">
                 <div class="container">
+
                     <div class="row">
                         <div class="col-md-3">
                             <a class="logo" href="index.html">
@@ -58,7 +60,9 @@
 
                         <!-- INI NAVBAR YA -->
                         <div class="col-md-4">
+
                             <div class="top-user-area clearfix">
+
                                 <ul class="top-user-area-list list list-horizontal list-border">
                                     <li class="top-user-area-avatar">
                                         <a href="#">Home</a>
@@ -73,8 +77,9 @@
                                     </li>
 
                                 </ul>
+                              </div>
                             </div>
-                        </div>
+
                         </div>
                     </div>
                 </div>
