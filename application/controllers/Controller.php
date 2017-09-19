001fd4ec0c5a67c2dd68973618551c2789902fe6
rizkiramadhan2 <rizki2500@gmail.com>
2017-09-19 17:28:14 +0700

    Update function get_harga

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -34,21 +34,30 @@ class Controller extends CI_Controller {
     public function get_kota($nama_kota){
     	$result= $this->Model->read('tb_kota', "nama_kota LIKE '$nama_kota%'");
     	$arrayName = array('data' => $result );
-    	//print_r($arrayName);
+    	//print_r($arrayName)
     	$this->load->view('kota', $arrayName);
     }
 
     public function get_harga(){
     	$arrayName['nama_kota'] = explode(',', $_GET['tujuan'])[0];
+    	$info = $this->Model->read('tb_kota', $arrayName);
 
-    	// var_dump($arrayName);
+    	if ($info[0]['ket']=='luar jawa') {
+    	$s = "SELECT DISTINCT k.id_kota, k.nama_kota, k.provinsi, l.tarif as 'tarif_laut', l.estimasi as 'estimasi2', u.tarif as 'tarif_udara', u.estimasi as 'estimasi3' FROM tb_kota k,tb_via_laut l,tb_via_udara u WHERE k.nama_kota ="."'".$arrayName['nama_kota']."'";
+    	}
+       	//INI TABLE JOIN
+    	else{
+    		$s = "SELECT DISTINCT k.id_kota, k.nama_kota, k.provinsi, d.tarif as 'tarif_darat', d.estimasi as 'estimasi1', l.tarif as 'tarif_laut', l.estimasi as 'estimasi2', u.tarif as 'tarif_udara', u.estimasi as 'estimasi3' FROM tb_kota k,tb_via_darat d,tb_via_laut l,tb_via_udara u WHERE k.nama_kota ="."'".$arrayName['nama_kota']."'";
+    	}
+    	$result = $this->db->query($s);
 
-    	$result = $this->Model->read('tb_kota', $arrayName);
-
-    	// var_dump($result);
-
-    	$data['result'] = $result;
-
+    	//MERGE VARIABEL GET BERAT DENGAN OBJEK RESULT SQL
+    	$newObj = new stdClass;
+    	$newObj->berat = $_GET['berat'];
+    	$new_result[0] = (object)array_merge((array)$newObj, (array)$result->result()[0]);
+    	//print_r($new_result[0]);
+    	//PASSING KE VIEW
+    	$data['result'] = $new_result;
     	$this->load->view('price_table', $data);
 
     }
