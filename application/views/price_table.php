1319a7c8688b737ad8394b24cedb1f94e755939d
rizkiramadhan2 <rizki2500@gmail.com>
2017-09-19 17:29:24 +0700

    update object json

--- application/views/price_table.php
+++ application/views/price_table.php
@@ -1,3 +1,34 @@
-<?php
-	var_dump($result);
-?>
+<?php foreach($result as $row): ?>
+	<?= 'Asal : Malang<br>Tujuan : '.$row->nama_kota.'<br>Berat : '.$row->berat?>
+<table class="table table-striped table-list-search animated fadeIn">
+  <tr>
+    <th>Jalur</th>
+    <th>Tarif</th> 
+    <th>Estimasi</th>
+  </tr>
+  <tr>
+    <td>Darat</td>
+    <td><?php if(isset($row->tarif_darat)){
+    	echo $row->tarif_darat*$row->berat;
+    	}else{
+    		echo '-';
+    		}?></td> 
+    <td><?php if(isset($row->estimasi1)){
+    	echo $row->estimasi1;
+    	}else{
+    		echo '-';
+    		}?></td>
+  </tr>
+  <tr>
+    <td>Laut</td>
+    <td><?= $row->tarif_laut*$row->berat?></td> 
+    <td><?= $row->estimasi2?></td>
+  </tr>
+  <tr>
+    <td>Udara</td>
+    <td><?= $row->tarif_udara*$row->berat?></td> 
+    <td><?= $row->estimasi3?></td>
+  </tr>
+</table>
+<?php endforeach ?>
+
